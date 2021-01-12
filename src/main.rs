5a5fa237f1f197ea9e40eabb6bc29f9f9b260e03
Jeff Zhang <zhang.jeffreyd@gmail.com>
2021-01-12 14:08:39 -0500

    Create basic structure for Torrent files
    Co-authored-by: Eshan Singh <eshansingh@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
 mod bencode_parser;
 
-fn main() {}
+use std::{env, fs, path::PathBuf};
+use snafu::Snafu;
+use bencode_parser::{Bencode, parse_bencode};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let torrent_bytes = fs::read(env::args().nth(1).ok_or("Didn't find a torrent file in the first argument")?)?;
+
+    let (_, torrent) = parse_bencode(&torrent_bytes).map_err(|_| "Invalid bencode")?;
+
+    parse_torrent(torrent)?;
+
+    Ok(())
+}
+
+fn parse_torrent(torrent: Bencode) -> Result<Torrent, TorrentParsingError> {
+    let torrent_dict = if let Bencode::Dict(dict) = torrent { dict } else {
+        return Err(TorrentParsingError::NotADictionary);
+    };
+
+    todo!()
+}
+
+pub struct Torrent {
+    announce: String,
+    encoding: Option<String>,
+    info: TorrentInfo,
+}
+
+type SHA1Hash = [u8; 20];
+
+pub struct TorrentInfo {
+    piece_len: u64,
+    pieces: Vec<SHA1Hash>,
+    name: String,
+    files: Vec<TorrentFile>,
+}
+
+pub struct TorrentFile {
+    length: u64,
+    path: PathBuf,
+}
+
+#[non_exhaustive]
+#[derive(Debug, Snafu)]
+pub enum TorrentParsingError {
+    #[snafu(display("Torrent bencode is not a dictionary"))]
+    NotADictionary,
+}
